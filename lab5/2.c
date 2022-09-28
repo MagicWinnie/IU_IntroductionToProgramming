f94de04c9381357f317c2b9eddcd683f76d96505
MagicWinnie <magicwinnie@yandex.ru>
2022-09-28 14:56:04 +0300

    added lab5 task 2

--- lab5/2.c
+++ lab5/2.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+struct fields
+{
+    unsigned version:4;
+    unsigned IHL:4;
+    unsigned DSCP:6;
+    unsigned ECN:2;
+    unsigned total_length:16; 
+};
+
+union IP
+{
+    struct fields field;
+    unsigned access;
+};
+
 int main()
 {
+    union IP ip;
+
+    printf("Enter header: ");
+    scanf("%d", &ip.access);
+
+    printf("Version: %d\n", ip.field.version);
+    printf("IHL: %d\n", ip.field.IHL);
+    printf("DSCP: %d\n", ip.field.DSCP);
+    printf("ECN: %d\n", ip.field.ECN);
+    printf("Total length: %d\n", ip.field.total_length);
     return 0;
 }
