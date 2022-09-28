23ab0e90203c7c6f4b0b377fa85bd8abf1b79e85
MagicWinnie <magicwinnie@yandex.ru>
2022-09-28 16:03:18 +0300

    added input from keyboard for lab5 task 4

--- lab5/4.c
+++ lab5/4.c
@@ -1,89 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define MAX_LEN 128
 
 typedef struct
 {
-    char name[128];
+    char name[MAX_LEN];
+    int amount;
+} INGREDIENT;
+
+typedef struct
+{
+    char name[MAX_LEN];
     int amount_ingredients;
-    struct { char name[128]; int amount; } ingredients[10];
+    INGREDIENT *ingredients;
 } RECIPE;
+
+void add_recipe(RECIPE *rec, char *name, int amount);
+void add_ingredient(INGREDIENT *ing, char *name, int amount);
 
 int main()
 {
-    RECIPE cookbook[3];
+    int n;
+    printf("How many recipes will be entered: ");
+    scanf("%d\n", &n);
+    RECIPE *cookbook = (RECIPE *)malloc(sizeof(RECIPE) * n);
 
-    /////// RECIPE #1 ///////
-    strcpy(cookbook[0].name, "tea");
-    cookbook[0].amount_ingredients = 2;
-    ////// INGREDIENT #1 //////
-    strcpy(cookbook[0].ingredients[0].name, "water");
-    cookbook[0].ingredients[0].amount = 300;
-    ////// INGREDIENT #2 //////
-    strcpy(cookbook[0].ingredients[1].name, "tea bag");
-    cookbook[0].ingredients[1].amount = 1;
-    /////// RECIPE #2 ///////
-    strcpy(cookbook[1].name, "pizza");
-    cookbook[1].amount_ingredients = 10;
-    ////// INGREDIENT #1 //////
-    strcpy(cookbook[1].ingredients[0].name, "flour");
-    cookbook[1].ingredients[0].amount = 300;
-    ////// INGREDIENT #2 //////
-    strcpy(cookbook[1].ingredients[1].name, "yeast");
-    cookbook[1].ingredients[1].amount = 1;
-    ////// INGREDIENT #3 //////
-    strcpy(cookbook[1].ingredients[2].name, "salt");
-    cookbook[1].ingredients[2].amount = 1;
-    ////// INGREDIENT #4 //////
-    strcpy(cookbook[1].ingredients[3].name, "olive oil");
-    cookbook[1].ingredients[3].amount = 1;
-    ////// INGREDIENT #5 //////
-    strcpy(cookbook[1].ingredients[4].name, "passata");
-    cookbook[1].ingredients[4].amount = 100;
-    ////// INGREDIENT #6 //////
-    strcpy(cookbook[1].ingredients[5].name, "basil");
-    cookbook[1].ingredients[5].amount = 1;
-    ////// INGREDIENT #7 //////
-    strcpy(cookbook[1].ingredients[6].name, "garlic");
-    cookbook[1].ingredients[6].amount = 1;
-    ////// INGREDIENT #8 //////
-    strcpy(cookbook[1].ingredients[7].name, "mozzarella");
-    cookbook[1].ingredients[7].amount = 125;
-    ////// INGREDIENT #9 //////
-    strcpy(cookbook[1].ingredients[8].name, "tomatoes");
-    cookbook[1].ingredients[8].amount = 100;
-    ////// INGREDIENT #10 //////
-    strcpy(cookbook[1].ingredients[9].name, "parmesan");
-    cookbook[1].ingredients[9].amount = 90;
-    /////// RECIPE #3 ///////
-    strcpy(cookbook[2].name, "pasta carbonara");
-    cookbook[2].amount_ingredients = 8;
-    ////// INGREDIENT #1 //////
-    strcpy(cookbook[2].ingredients[0].name, "pancetta");
-    cookbook[2].ingredients[0].amount = 100;
-    ////// INGREDIENT #2 //////
-    strcpy(cookbook[2].ingredients[1].name, "pecorino cheese");
-    cookbook[2].ingredients[1].amount = 50;
-    ////// INGREDIENT #3 //////
-    strcpy(cookbook[2].ingredients[2].name, "parmesan");
-    cookbook[2].ingredients[2].amount = 50;
-    ////// INGREDIENT #4 //////
-    strcpy(cookbook[2].ingredients[3].name, "eggs");
-    cookbook[2].ingredients[3].amount = 3;
-    ////// INGREDIENT #5 //////
-    strcpy(cookbook[2].ingredients[4].name, "spaghetti");
-    cookbook[2].ingredients[4].amount = 350;
-    ////// INGREDIENT #6 //////
-    strcpy(cookbook[2].ingredients[5].name, "garlic");
-    cookbook[2].ingredients[5].amount = 2;
-    ////// INGREDIENT #7 //////
-    strcpy(cookbook[2].ingredients[6].name, "unsalted butter");
-    cookbook[2].ingredients[6].amount = 50;
-    ////// INGREDIENT #8 //////
-    strcpy(cookbook[2].ingredients[7].name, "salt");
-    cookbook[2].ingredients[7].amount = 1;
+    for (int i = 0; i < n; i++)
+    {
+        int amount_ing;
+        char name[MAX_LEN];
+        printf("Enter name of dish: ");
+        fgets(name, MAX_LEN, stdin);
+        name[strcspn(name, "\n")] = 0;
+        printf("Enter amount of ingredients: ");
+        scanf("%d\n", &amount_ing);
+        add_recipe(&cookbook[i], name, amount_ing);
+        for (int j = 0; j < amount_ing; j++)
+        {
+            int amount;
+            char name_ing[MAX_LEN];
+            printf("Enter name of ingredient: ");
+            fgets(name_ing, MAX_LEN, stdin);
+            name_ing[strcspn(name_ing, "\n")] = 0;
+            printf("Enter amount of ingredient: ");
+            scanf("%d\n", &amount);
+            add_ingredient(&cookbook[i].ingredients[j], name_ing, amount);
+        }
+        printf("HERE\n");
+    }
 
     printf("RECIPES:\n");
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("\tRECIPE #%d:\n", i + 1);
         printf("\t\tName: %s\n", cookbook[i].name);
@@ -95,5 +63,71 @@ int main()
             printf("\t\t\t\tAmount: %d\n", cookbook[i].ingredients[j].amount);
         }
     }
+    for (int i = 0; i < n; i++)
+        free(cookbook[i].ingredients);
+    free(cookbook);
     return 0;
 }
+
+void add_recipe(RECIPE *rec, char *name, int amount)
+{
+    strcpy(rec->name, name);
+    rec->amount_ingredients = amount;
+    rec->ingredients = (INGREDIENT *)malloc(sizeof(INGREDIENT) * amount);
+}
+
+void add_ingredient(INGREDIENT *ing, char *name, int amount)
+{
+    strcpy(ing->name, name);
+    ing->amount = amount;
+}
+
+/*
+3
+tea
+2
+water
+300
+tea bag
+1
+pizza
+10
+flour
+300
+yeast
+300
+salt
+1
+olive oil
+1
+passata
+100
+basil
+1
+garlic
+1
+mozzarella
+125
+tomatoes
+100
+parmesan
+90
+pasta carbonara
+8
+pancetta
+100
+pecorino cheese
+50
+parmesan
+50
+eggs
+3
+spaghetti
+350
+garlic
+2
+unsalted butter
+50
+salt
+1
+*/
