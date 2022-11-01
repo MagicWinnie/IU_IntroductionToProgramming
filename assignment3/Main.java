fada3a03d30b5b4c25446595ef780f2b591a7931
MagicWinnie <magicwinnie@yandex.ru>
2022-11-01 21:26:39 +0300

    Added assignment 3

---
+++ assignment3/Main.java
@@ -0,0 +1,382 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Main class.
+ */
+public class Main {
+    /**
+     * Lower bound for n.
+     */
+    private final int nLowerBound = 1;
+    /**
+     * Upper bound for n.
+     */
+    private final int nUpperBound = 50;
+    /**
+     * Calculator object with default calculator.
+     */
+    private Calculator calc;
+
+    /**
+     * This method reads the operand types and parses them.
+     * @param scanner java util scanner
+     * @return operand type that has been read
+     */
+    private CalculatorType readCalculator(Scanner scanner) {
+        String type = scanner.nextLine(); // read calculator type
+        CalculatorType calcType;
+        switch (type) {
+            case "DOUBLE":
+                calcType = CalculatorType.DOUBLE;
+                this.calc = new DoubleCalculator();
+                break;
+            case "INTEGER":
+                calcType = CalculatorType.INTEGER;
+                this.calc = new IntegerCalculator();
+                break;
+            case "STRING":
+                calcType = CalculatorType.STRING;
+                this.calc = new StringCalculator();
+                break;
+            default:
+                calcType = CalculatorType.INCORRECT;
+                this.reportFatalError("Wrong calculator type");
+                break;
+        }
+        return calcType;
+    }
+
+    /**
+     * This method reads amount of commands.
+     * @param scanner java util scanner
+     * @return number of commands
+     */
+    private int readCommandsNumber(Scanner scanner) {
+        int n = 0;
+        try {
+            n = scanner.nextInt();
+        } catch (NumberFormatException ex) {
+            this.reportFatalError("Amount of commands is Not a Number");
+        } catch (InputMismatchException ex) {
+            this.reportFatalError("Amount of commands is Not a Number");
+        }
+        if (n < this.nLowerBound || n > this.nUpperBound) {
+            this.reportFatalError("Amount of commands is Not a Number");
+        }
+        return n;
+    }
+
+    /**
+     * This method prints error to console and exits program.
+     * @param err Error string
+     */
+    private void reportFatalError(String err) {
+        System.out.println(err);
+        System.exit(0);
+    }
+
+    /**
+     * This method return OperationType enum field based on String.
+     * @param operation operation string
+     * @return operation enum
+     */
+    private OperationType parseOperation(String operation) {
+        OperationType operType;
+        switch (operation) {
+            case "+":
+                operType = OperationType.ADDITION;
+                break;
+            case "-":
+                operType = OperationType.SUBTRACTION;
+                break;
+            case "/":
+                operType = OperationType.DIVISION;
+                break;
+            case "*":
+                operType = OperationType.MULTIPLICATION;
+                break;
+            default:
+                operType = OperationType.INCORRECT;
+                break;
+        }
+        return operType;
+    }
+
+    /**
+     * This method reads command line and processes it.
+     * @param scanner java util scanner
+     */
+    private void calculate(Scanner scanner) {
+        String command = scanner.next();
+        String operand1 = scanner.next();
+        String operand2 = scanner.next();
+        OperationType operType = this.parseOperation(command);
+        switch (operType) {
+            case ADDITION:
+                System.out.println(calc.add(operand1, operand2));
+                break;
+            case SUBTRACTION:
+                System.out.println(calc.substract(operand1, operand2));
+                break;
+            case MULTIPLICATION:
+                System.out.println(calc.multiply(operand1, operand2));
+                break;
+            case DIVISION:
+                System.out.println(calc.divide(operand1, operand2));
+                break;
+            default:
+                System.out.println("Wrong operation type");
+                break;
+        }
+    }
+    /**
+     * The main method.
+     * It reads the inputs.
+     * @param args execution arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Main my = new Main();
+        my.readCalculator(scanner);
+        int n = my.readCommandsNumber(scanner);
+        for (int i = 0; i < n; i++) {
+            my.calculate(scanner);
+        }
+        scanner.close();
+    }
+}
+
+/**
+ * Calculator types.
+ */
+enum CalculatorType {
+    /**
+     * Calculator works with integer operands.
+     */
+    INTEGER,
+    /**
+     * Calculator works with double operands.
+     */
+    DOUBLE,
+    /**
+     * Calculator works with string operands.
+     */
+    STRING,
+    /**
+     * Invalid operand type.
+     */
+    INCORRECT
+}
+
+/**
+ * Operation types.
+ */
+enum OperationType {
+    /**
+     * Addition operation.
+     */
+    ADDITION,
+    /**
+     * Subtraction operation.
+     */
+    SUBTRACTION,
+    /**
+     * Multiplication operation.
+     */
+    MULTIPLICATION,
+    /**
+     * Division operation.
+     */
+    DIVISION,
+    /**
+     * Incorrect operation.
+     */
+    INCORRECT
+}
+
+/**
+ * Abstract class for Calculator of different types.
+ */
+abstract class Calculator {
+    /**
+     * This method calculates result of a + b.
+     * @param a String for operand 1.
+     * @param b String for operand 2.
+     * @return Returns a string, that is a result of a + b or error message if unsupported.
+     */
+    public abstract String add(String a, String b);
+    /**
+     * This method calculates result of a - b.
+     * @param a String for operand 1.
+     * @param b String for operand 2.
+     * @return Returns a string, that is a result of a + b or error message if unsupported.
+     */
+    public abstract String substract(String a, String b);
+    /**
+     * This method calculates result of a * b.
+     * @param a String for operand 1.
+     * @param b String for operand 2.
+     * @return Returns a string, that is a result of a + b or error message if unsupported.
+     */
+    public abstract String multiply(String a, String b);
+    /**
+     * This method calculates result of a / b.
+     * @param a String for operand 1.
+     * @param b String for operand 2.
+     * @return Returns a string, that is a result of a + b or error message if unsupported.
+     */
+    public abstract String divide(String a, String b);
+
+    /**
+     * This method checks if string is integer type.
+     * @param n String that probably contains an integer.
+     * @return True if it is an integer else False
+     */
+    protected boolean isInteger(String n) {
+        try {
+            Integer.parseInt(n);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+
+    /**
+     * This method checks if string is double type.
+     * @param n String that probably contains an double.
+     * @return True if it is an double else False
+     */
+    protected boolean isDouble(String n) {
+        try {
+            Double.parseDouble(n);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+
+    /**
+     * This method checks if string is integer or double type.
+     * @param n String that probably contains an integer or double.
+     * @return True if it is an integer or double else False
+     */
+    protected boolean isNumber(String n) {
+        return this.isInteger(n) || this.isDouble(n);
+    }
+}
+
+/**
+ * Class for integer calculator.
+ */
+class IntegerCalculator extends Calculator {
+    public String add(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        int operand1 = Integer.parseInt(a);
+        int operand2 = Integer.parseInt(b);
+        int result = operand1 + operand2;
+        return Integer.toString(result);
+    }
+    public String substract(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        int operand1 = Integer.parseInt(a);
+        int operand2 = Integer.parseInt(b);
+        int result = operand1 - operand2;
+        return Integer.toString(result);
+    }
+    public String multiply(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        int operand1 = Integer.parseInt(a);
+        int operand2 = Integer.parseInt(b);
+        int result = operand1 * operand2;
+        return Integer.toString(result);
+    }
+    public String divide(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        int operand1 = Integer.parseInt(a);
+        int operand2 = Integer.parseInt(b);
+        if (operand2 == 0) {
+            return "Division by zero";
+        }
+        int result = operand1 / operand2;
+        return Integer.toString(result);
+    }
+}
+
+/**
+ * Class for double calculator.
+ */
+class DoubleCalculator extends Calculator {
+    public String add(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        double operand1 = Double.parseDouble(a);
+        double operand2 = Double.parseDouble(b);
+        double result = operand1 + operand2;
+        return Double.toString(result);
+    }
+    public String substract(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        double operand1 = Double.parseDouble(a);
+        double operand2 = Double.parseDouble(b);
+        double result = operand1 - operand2;
+        return Double.toString(result);
+    }
+    public String multiply(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        double operand1 = Double.parseDouble(a);
+        double operand2 = Double.parseDouble(b);
+        double result = operand1 * operand2;
+        return Double.toString(result);
+    }
+    public String divide(String a, String b) {
+        if (!this.isNumber(a) || !this.isNumber(b)) {
+            return "Wrong argument type";
+        }
+        double operand1 = Double.parseDouble(a);
+        double operand2 = Double.parseDouble(b);
+        if (operand2 == 0D) {
+            return "Division by zero";
+        }
+        double result = operand1 / operand2;
+        return Double.toString(result);
+    }
+}
+
+/**
+ * Class for string calculator.
+ */
+class StringCalculator extends Calculator {
+    public String add(String a, String b) {
+        return a + b;
+    }
+    public String substract(String a, String b) {
+        return "Unsupported operation for strings";
+    }
+    public String multiply(String a, String b) {
+        if (!isInteger(b) || (isInteger(b) && Integer.parseInt(b) < 1)) {
+            return "Wrong argument type";
+        }
+        String s = "";
+        for (int i = 0; i < Integer.parseInt(b); i++) {
+            s += a;
+        }
+        return s;
+    }
+    public String divide(String a, String b) {
+        return "Unsupported operation for strings";
+    }
+}
